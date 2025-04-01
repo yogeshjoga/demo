dfb90a052a8e5bc46bad0aeab62378f02b492c7d
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2025-04-01 16:56:38 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import login from './components/login';
-function App() {
+import Login from './components/Login';
+import H from './components/H';
 
+function App() {
 
   return (
     <>
     <div>
       <h1>Hello this is my first reactJs app</h1>
     </div>
+    <H/>
     <Router>
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/login" element={<login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/h" element = {<H/>}/> 
       </Routes>
     </Router>
+
+
+
+
 
     </>
   )
 }
 
 export default App
+
+
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 
 function login(){
-    const [username, setUsername] = useState("Hello User");
-    const [password, setPassword] = useState("Hello passwrods");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     return(
         <div className="login">
             <h1>Login</h1>
             <h2>Login to your account</h2>
             <form>
-                <label for="username">Username:</label><br/>
+                <label for="username" style={{ marginRight: "10px" }}>Username:</label>
                 <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} /><br/>
-                <label for="password">Password:</label><br/>
+                <label for="password" style={{ marginRight: "10px" }}>Password:</label>
                 <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br/><br/>
                 <input type="submit" value="Login" />
             </form>
+
+
+
+            
         </div>
     )
 }
